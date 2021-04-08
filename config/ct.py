c0ee134e871add179cc502b6c40975dc2214d7c4
Mohamed Dief <mdaif1332@gmail.com>
2021-04-08 20:40:12 +0200

    Update ct.py

--- config/ct.py
+++ config/ct.py
@@ -6,5 +6,23 @@ the files it's serving
 
 __CT__ = {
     "js": "text/x-javascript",
-    "css": "text/css"
+    "css": "text/css",
+    "txt": "text/plain",
+    "avi": "video/x-msvideo",
+    "gif": "image/gif",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "json": "application/json",
+    "mp3": "audio/mpeg",
+    "mp4": "video/mp4",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "tiff": "image/tiff",
+    "tif": "image/tiff",
+    "ttf": "font/ttf",
+    "wav": "audio/wav",
+    "webm": "video/webm",
+    "woff": "font/woff",
+    "woff2": "font/woff2",
+    "xml": "application/xml"
 }
