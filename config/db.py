8824a7fc0aff456ebe95306c17d6bdbf91780b8c
Mohamed Dief <mdaif1332@gmail.com>
2021-05-08 22:18:07 +0200

    Admin default DB config

--- config/db.py
+++ config/db.py
@@ -1,1 +1,14 @@
 __DB_BASE_FOLDER__ = "dbs/"
+
+__DEFAULT_DB_NAME__ = "main.db"
+__DEFAULT_DB_TABLE__ = "admins"
+
+__DEFAULT_DB_CONFIG__ = {
+    "first_name":"test",
+    "second_name":"text",
+    "username":"text",
+    "password":"text",
+    "email":"text",
+    "phone":"text",
+    "session_id":"text"
+}
