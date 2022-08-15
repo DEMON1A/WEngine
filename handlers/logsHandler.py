a81f45d657ff4cc64ca34cb9f1a04ecf7234564c
Mohamed Dief <mdaif1332@gmail.com>
2022-08-14 19:27:27 -0700

    Allow 127.0.0.1 to access logs

--- handlers/logsHandler.py
+++ handlers/logsHandler.py
@@ -5,5 +5,8 @@ from config.settings import PUBLIC_LOGS
 def Handler(requestHeaders):
     if PUBLIC_LOGS:
         return returnHTTPBasicResponse(readLogs(), {"content-type":"text/plain"}, 200)
+    elif requestHeaders['client-ip'] == "127.0.0.1":
+        # allow only localhost to acess this when denied
+        return returnHTTPBasicResponse(readLogs(), {"content-type":"text/plain"}, 200)
     else:
         return returnRenderedTemplate('WEngine/defaultPages/403.html', {}, 403)
